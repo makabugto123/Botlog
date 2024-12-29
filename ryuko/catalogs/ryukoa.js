e21bb1644f256606c28e0df4c933e1749c2cb05a
makabugto123 <139425756+makabugto123@users.noreply.github.com>
2024-12-29 09:08:13 +0800

    Update ryukoa.js

--- ryuko/catalogs/ryukoa.js
+++ ryuko/catalogs/ryukoa.js
@@ -5,7 +5,7 @@ const app = express();
 const chalk = require('chalk');
 const logger = require("./ryukoc.js");
 const path = require('path');
-const PORT = process.env.PORT || 8080 || 9000 || 5555 || 5050 || 5000 || 3003 || 2000 || 1029 || 1010;
+const PORT = process.env.PORT || 3000;
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/website/ryuko.html'));
 });
